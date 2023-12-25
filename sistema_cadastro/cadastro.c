807e018a47d451312f8af9b3f371058c7db595a2
LuisFcamo <luis1170da@gmail.com>
2023-12-25 19:29:23 -0300

    aaaaa

--- sistema_cadastro/cadastro.c
+++ sistema_cadastro/cadastro.c
@@ -4,39 +4,92 @@
 
 GtkBuilder *builder;
 GtkWidget *window;
+GtkStack *stack;
+GtkListStore *modelo_armazenamento;
 
+typedef struct usuario{
+    int id;
+    char name[100];
+    char email[100];
+    struct usuario *prox;
+} user;
+
+
+int id = 0;
+user *cabecelho_user;
+user *proximo_user;
 
 void on_mainwindow_destroy () {
     gtk_main_quit();
 }
 
 void on_bt_listar_clicked () {
+    user *refp = proximo_user;
+    refp -> prox = NULL;
+    char nome[100];
+    char email[100];
 
+    GtkTreeIter *iter;
+    gtk_list_store_clear(modelo_armazenamento);
+    //apontando para o começo da lista encadeada
+    refp = cabecelho_user;
+    for (int i = 0; i < id; i++) {
+        gtk_list_store_append(modelo_armazenamento, &iter);
+        gtk_list_store_set(modelo_armazenamento, &iter,
+            0, (refp -> id),
+            1, (refp -> name),
+            2, (refp -> email),
+            -1 );
+        
+        refp = refp -> prox;
+    }
 }
 
 void on_bt_listar_voltar_clicked () {
-
+    gtk_stack_set_visible_child_name(stack, "view_init");
 }
 
 void on_bt_cadastrar_clicked () {
+    char *cad_nome = gtk_entry_get_text(gtk_builder_get_object(builder, "cad_nome"));
+    char *cad_email = gtk_entry_get_text(gtk_builder_get_object(builder, "cad_email"));
+    
+    if (strcmp(cad_nome, "") == 0) {
+        mensagem("aviso", "campo nome obrigatorio", "dialog error");
+    } else {
+        id++;
+        proximo_user -> id = id;
+        strcpy(proximo_user -> name, cad_nome);
+        strcpy(proximo_user -> email, cad_email);
+        
+        char texto[100];
 
+        g_snprintf(texto, 100, "%s%s%s", "Usuario ", proximo_user->name, " cadastrado");
+        mensagem("aviso", texto, "dialog-emblem-defalt");
+
+        proximo_user -> prox = (user *) malloc(sizeof(user)*1);
+        proximo_user = proximo_user -> prox;
+
+    }
 }
 
 void on_bt_cadvoltar_clicked () {
-
+    gtk_stack_set_visible_child_name(stack, "view_init");
 }
 
 void on_sair_clicked () {
+    gtk_stack_set_visible_child_name(stack, "view_login");
+    g_object_set(window, "icon-name", "changes_prevent", NULL);
 
 }
 
 void on_listar_clicked () {
-
+    gtk_stack_set_visible_child_name(stack, "view_listar");
 }
 
 void on_cadastrar_clicked () {
-
+    gtk_stack_set_visible_child_name(stack, "view_cad");
 }
+
 void mensagem (char text[100], char secundary[100], char icon_name[100]) {
     GtkMessageDialog *mensagem_dialogo = gtk_builder_get_object(builder, "msg");
     g_object_set(mensagem_dialogo, "text", text, NULL);
@@ -52,6 +105,8 @@ void mensagem (char text[100], char secundary[100], char icon_name[100]) {
 void login (char *email, char *senha, bool *lembrar) {
     if (strcmp(email, "admin") == 0 && strcmp(senha, "admin") == 0) {
         mensagem("bem-vindo", "usuario logado com sucesso", "dialog-ok");
+        gtk_stack_set_visible_child_name(stack, "view_init");
+        g_object_set(window, "icon-name", "avatar-default", NULL);
     } else {
         mensagem("aviso", "email-ou-senha incorretos", "dialog-error");
     }
@@ -65,6 +120,9 @@ void on_login_clicked () {
 }
 
 int main (int argc, char *agrv[]) {
+    cabecelho_user = (user*) malloc(sizeof(user)*1);
+    proximo_user = cabecelho_user;
+
     gtk_init(&argc, &agrv);
 
     builder = gtk_builder_new_from_file("sistemacadastro.glade");
@@ -83,6 +141,8 @@ int main (int argc, char *agrv[]) {
 
     gtk_builder_connect_signals(builder, NULL);
 
+    stack = GTK_STACK(gtk_builder_get_object(builder, "stack"));
+    modelo_armazenamento = GTK_LIST_STORE(gtk_builder_get_object(builder, "liststore1"));
     window = GTK_WIDGET(gtk_builder_get_object(builder, "mainwindow"));
 
     gtk_widget_show_all(window);
